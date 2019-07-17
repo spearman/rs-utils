af88af932a7a8e104cc22744b20bb162f1fc8561
Shane Pearman <spearman@gitlab.com>
2019-07-16 23:08:05 -0700

    rand example, update dependencies

--- examples/example.rs
+++ examples/example.rs
@@ -1,45 +1,41 @@
 #[macro_use] extern crate unwrap;
 
+extern crate rand;
 extern crate tempdir;
 
 extern crate rs_utils;
+
+use rs_utils::*;
 
 fn main () {
   let exe_name = std::env::current_exe().unwrap().file_name().unwrap()
     .to_owned();
   println!("{:?} main...", exe_name);
 
+  println!("{}", String::from_utf8 (vec![b'#'; 80]).unwrap());
+  println!("example: incrementally named files");
   let temp_dir = unwrap!{ tempdir::TempDir::new_in ("examples", "tmp") };
-
   let file_path = temp_dir.path().join (
     std::path::Path::new ("myfile"));
   let file_path = file_path.as_path();
-
-  println!("file_path_incremental \"{}\": {}", file_path.display(),
-    unwrap!{ rs_utils::file::file_path_incremental (file_path) }.display()
-  );
+  println!("file_path_incremental\n  \"{}\":\n  {}", file_path.display(),
+    unwrap!{ file::file_path_incremental (file_path) }.display());
   println!("file_new_append_incremental \"{}\"...", file_path.display());
-  unwrap!{ rs_utils::file::file_new_append_incremental (file_path) };
-
-  println!("file_path_incremental \"{}\": {}", file_path.display(),
-    unwrap!{ rs_utils::file::file_path_incremental (file_path) }.display()
-  );
+  unwrap!{ file::file_new_append_incremental (file_path) };
+  println!("file_path_incremental\n  \"{}\":\n  {}", file_path.display(),
+    unwrap!{ file::file_path_incremental (file_path) }.display());
   println!("file_new_append_incremental \"{}\"...", file_path.display());
-  unwrap!{ rs_utils::file::file_new_append_incremental (file_path) };
-
-  println!("file_path_incremental \"{}\": {}", file_path.display(),
-    unwrap!{ rs_utils::file::file_path_incremental (file_path) }.display()
-  );
-  println!("file_path_incremental and then file_new_append \"{}\"...",
-    file_path.display()
-  );
+  unwrap!{ file::file_new_append_incremental (file_path) };
+  println!("file_path_incremental\n  \"{}\":\n  {}", file_path.display(),
+    unwrap!{ file::file_path_incremental (file_path) }.display());
+  println!("file_path_incremental and then file_new_append\n  \"{}\"...",
+    file_path.display());
   unwrap!{
     // file_new_append_incremental is equivalent to:
-    rs_utils::file::file_path_incremental (file_path).and_then (
-      |path_buf| rs_utils::file::file_new_append (path_buf.as_path())
+    file::file_path_incremental (file_path).and_then (
+      |path_buf| file::file_new_append (path_buf.as_path())
     )
   };
-
   println!("!ls {}", temp_dir.path().display());
   assert!{
     unwrap!{
@@ -47,6 +43,13 @@ fn main () {
         .status()
     }.success()
   };
+
+  println!("{}", String::from_utf8 (vec![b'#'; 80]).unwrap());
+  println!("example: default xorshift rng");
+  use rand::Rng;
+  let mut xorshiftrng = numeric::xorshift_rng_unseeded();
+  let b = xorshiftrng.gen::<u8>();
+  println!("b: {:?}", b);
 
   println!("...{:?} main", exe_name);
 }
