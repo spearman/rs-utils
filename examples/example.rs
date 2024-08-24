31542597060234d10152f37601639ef04c5e9e0b
Shane Pearman <spearman@gitlab.com>
2024-08-24 18:14:10 +0200

    0.1.0: add log kv module and env_logger format functions

--- examples/example.rs
+++ examples/example.rs
@@ -13,25 +13,38 @@ fn main () {
   env_logger::Builder::new()
     .filter_level (LevelFilter::Debug)
     .parse_default_env()
+    //.format (log_::env_logger_json_format)
+    .format (log_::env_logger_custom_format)
     .init();
+  // test kv string
+  log_::kv::info!("test kv string"; a=r#"hello"world""#);
+  log_::kv::info!("test kv string"; a=r#"hello "world""#);
   // incremental files example
   log_::info!("example: incrementally named files");
   let temp_dir  =
     tempfile::Builder::new().prefix ("tmp").tempdir_in ("examples").unwrap();
   let file_path = temp_dir.path().join (Path::new ("myfile"));
   let path      = file_path.as_path();
-  log_::debug!("file_path_incremental\n  \"{}\":\n  {}",
-    path.display(), file::file_path_incremental (path).unwrap().display());
-  log_::debug!("file_new_append_incremental \"{}\"...", path.display());
+  log_::kv::debug!("file path incremental";
+    target_path=path.display().to_string().as_str(),
+    incremental_path=file::file_path_incremental (path).unwrap().display()
+      .to_string().as_str());
+  log_::kv::debug!("file new append incremental";
+    target_path=path.display().to_string().as_str());
   file::file_new_append_incremental (path).unwrap();
-  log_::debug!("file_path_incremental\n  \"{}\":\n  {}",
-    path.display(), file::file_path_incremental (path).unwrap().display());
-  log_::debug!("file_new_append_incremental \"{}\"...", path.display());
+  log_::kv::debug!("file path incremental";
+    target_path=path.display().to_string().as_str(),
+    incremental_path=file::file_path_incremental (path).unwrap().display()
+      .to_string().as_str());
+  log_::kv::debug!("file new append incremental";
+    target_path=path.display().to_string().as_str());
   file::file_new_append_incremental (path).unwrap();
-  log_::debug!("file_path_incremental\n  \"{}\":\n  {}",
-    path.display(), file::file_path_incremental (path).unwrap().display());
-  log_::debug!("file_path_incremental and then file_new_append\n  \"{}\"...",
-    path.display());
+  log_::kv::debug!("file path incremental";
+    target_path=path.display().to_string().as_str(),
+    incremental_path=file::file_path_incremental (path).unwrap().display()
+      .to_string().as_str());
+  log_::kv::debug!("file path incremental and then file new append";
+    target_path=path.display().to_string().as_str());
   // file_new_append_incremental is equivalent to:
   file::file_path_incremental (path)
     .and_then (|path_buf| file::file_new_append (path_buf.as_path())).unwrap();
--- src/log.rs
+++ src/log.rs
@@ -3,6 +3,9 @@
 #[doc(hidden)] extern crate log;
 #[doc(hidden)] extern crate stdext;
 
+#[cfg(feature="env-logger-format")]
+pub use self::kv::{env_logger_custom_format, env_logger_json_format};
+
 /// Log trace message that includes the enclosing function name in the target
 pub macro trace {
   ($($args:tt)*) => {
@@ -35,5 +38,110 @@ pub macro warn {
 pub macro error {
   ($($args:tt)*) => {
     log::error!(target: stdext::function_name!(), $($args)*)
+  }
+}
+
+pub mod kv {
+  //! The usual key-value format for log macros is to put the keys before the
+  //! message, the macros in this module reverses this order so that the log
+  //! message comes before the key/value pairs.
+
+  pub macro trace {
+    ($fmtstring:expr$(, $fmtarg:expr)*$(; $($kvargs:tt)*)?) => {
+      super::trace!($($($kvargs)*;)? $fmtstring$(, $fmtarg)*)
+    }
+  }
+  pub macro debug {
+    ($fmtstring:expr$(, $fmtarg:expr)*$(; $($kvargs:tt)*)?) => {
+      super::debug!($($($kvargs)*;)? $fmtstring$(, $fmtarg)*)
+    }
+  }
+  pub macro info {
+    ($fmtstring:expr$(, $fmtarg:expr)*$(; $($kvargs:tt)*)?) => {
+      super::info!($($($kvargs)*;)? $fmtstring$(, $fmtarg)*)
+    }
+  }
+  pub macro warn {
+    ($fmtstring:expr$(, $fmtarg:expr)*$(; $($kvargs:tt)*)?) => {
+      super::warn!($($($kvargs)*;)? $fmtstring$(, $fmtarg)*)
+    }
+  }
+  pub macro error {
+    ($fmtstring:expr$(, $fmtarg:expr)*$(; $($kvargs:tt)*)?) => {
+      super::error!($($($kvargs)*;)? $fmtstring$(, $fmtarg)*)
+    }
+  }
+
+  #[cfg(feature="env-logger-format")]
+  /// Formats log messages as a json object string
+  pub fn env_logger_json_format (
+    buf    : &mut env_logger::fmt::Formatter,
+    record : &log::Record
+  ) -> std::io::Result<()> {
+    use std::io::Write;
+    #[derive(Default)]
+    struct KVVisitor(pub String);
+    impl <'kvs> log::kv::VisitSource <'kvs> for KVVisitor {
+      fn visit_pair (&mut self,
+        key : log::kv::Key <'kvs>, value : log::kv::Value <'kvs>
+      ) -> Result <(), log::kv::Error> {
+        self.0 += format!(",\"{}\":{}", key, serde_json::to_string(&value).unwrap())
+          .as_str();
+        Ok(())
+      }
+    }
+    let mut kvv = KVVisitor::default();
+    record.key_values().visit (&mut kvv).unwrap();
+    let thread = std::thread::current().name().map (str::to_string)
+      .unwrap_or_else (||
+        format!("{:?}",
+          std::thread::current().id()).replace ("ThreadId", "unnamed"));
+    writeln!(buf,
+      "{{\"ts\":\"{}\",\"level\":\"{}\",\"thread\":\"{}\",\
+        \"target\":\"{}\",\"file\":\"{}:{}\",\"msg\":\"{}\"{}}}",
+      buf.timestamp(), record.level(), thread, record.target(),
+      record.file().unwrap(), record.line().unwrap(), record.args(), kvv.0
+    )
+  }
+
+  #[cfg(feature="env-logger-format")]
+  /// A custom `env_logger` format adding thread, target, and file information:
+  /// ```text
+  /// <ts> <level> <thread> <target> <file>: <msg> [ <key>=<value>]
+  /// ```
+  pub fn env_logger_custom_format (
+    buf    : &mut env_logger::fmt::Formatter,
+    record : &log::Record
+  ) -> std::io::Result<()> {
+    use std::io::Write;
+    #[derive(Default)]
+    struct KVVisitor(pub String);
+    impl <'kvs> log::kv::VisitSource <'kvs> for KVVisitor {
+      fn visit_pair (&mut self,
+        key : log::kv::Key <'kvs>, value : log::kv::Value <'kvs>
+      ) -> Result <(), log::kv::Error> {
+        let mut value_string = serde_json::to_string (&value).unwrap();
+        let mut value_str = value_string.as_str();
+        if !value_str.contains (char::is_whitespace) {
+          // if there is now whitespace we can remove outer quotes and un-escape
+          // any internal quotes
+          value_string = value_str.replace("\\\"", "\"");
+          value_str = &value_string[1..value_string.len()-1];
+        }
+        self.0 += format!(" {}={}", key, value_str).as_str();
+        Ok(())
+      }
+    }
+    let mut kvv = KVVisitor::default();
+    record.key_values().visit (&mut kvv).unwrap();
+    let thread = std::thread::current().name().map (str::to_string)
+      .unwrap_or_else (||
+        format!("{:?}",
+          std::thread::current().id()).replace ("ThreadId", "unnamed"));
+    writeln!(buf,
+      "{} {:5} {} {} {}:{}: {} {}",
+      buf.timestamp(), record.level(), thread, record.target(),
+      record.file().unwrap(), record.line().unwrap(), record.args(), kvv.0
+    )
   }
 }
