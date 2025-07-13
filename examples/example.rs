bdfa9da70d22f30d4885a7a5b8eaafa5b9265f2b
Shane Pearman <spearman@gitlab.com>
2025-07-13 04:14:05 +0200

    0.2.1: rename package, fix example, publish

--- examples/example.rs
+++ examples/example.rs
@@ -2,53 +2,54 @@
 
 use std::path::Path;
 use env_logger;
-use log::LevelFilter;
 use tempfile;
 
-// log conflicts with the log crate
-use rs_utils::{log as log_, file};
+use rs_utils::file;
+use rs_utils::log::{LevelFilter, kv as log};
 
 fn main () {
   println!("main...");
   env_logger::Builder::new()
     .filter_level (LevelFilter::Debug)
     .parse_default_env()
-    //.format (log_::env_logger_json_format)
-    .format (log_::env_logger_custom_format)
+    .format (
+      log::env_logger_custom_formatter (log::EnvLoggerFormatConfig::default()))
+    //.format (
+    //  log::env_logger_json_formatter (log::EnvLoggerFormatConfig::default()))
     .init();
   // test kv string
-  log_::kv::info!("test kv string"; a=r#"hello"world""#);
-  log_::kv::info!("test kv string"; a=r#"hello "world""#);
+  log::info!("test kv string"; a=r#"hello"world""#);
+  log::info!("test kv string"; a=r#"hello "world""#);
   // incremental files example
-  log_::info!("example: incrementally named files");
+  log::info!("example: incrementally named files");
   let temp_dir  =
     tempfile::Builder::new().prefix ("tmp").tempdir_in ("examples").unwrap();
   let file_path = temp_dir.path().join (Path::new ("myfile"));
   let path      = file_path.as_path();
-  log_::kv::debug!("file path incremental";
+  log::debug!("file path incremental";
     target_path=path.display().to_string().as_str(),
     incremental_path=file::file_path_incremental (path).unwrap().display()
       .to_string().as_str());
-  log_::kv::debug!("file new append incremental";
+  log::debug!("file new append incremental";
     target_path=path.display().to_string().as_str());
   file::file_new_append_incremental (path).unwrap();
-  log_::kv::debug!("file path incremental";
+  log::debug!("file path incremental";
     target_path=path.display().to_string().as_str(),
     incremental_path=file::file_path_incremental (path).unwrap().display()
       .to_string().as_str());
-  log_::kv::debug!("file new append incremental";
+  log::debug!("file new append incremental";
     target_path=path.display().to_string().as_str());
   file::file_new_append_incremental (path).unwrap();
-  log_::kv::debug!("file path incremental";
+  log::debug!("file path incremental";
     target_path=path.display().to_string().as_str(),
     incremental_path=file::file_path_incremental (path).unwrap().display()
       .to_string().as_str());
-  log_::kv::debug!("file path incremental and then file new append";
+  log::debug!("file path incremental and then file new append";
     target_path=path.display().to_string().as_str());
   // file_new_append_incremental is equivalent to:
   file::file_path_incremental (path)
     .and_then (|path_buf| file::file_new_append (path_buf.as_path())).unwrap();
-  log_::info!("!ls {}", temp_dir.path().display());
+  log::info!("!ls {}", temp_dir.path().display());
   assert!(std::process::Command::new ("ls")
     .arg (temp_dir.path().as_os_str())
     .status().unwrap().success());
---
+++ run-example.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cargo run --example example --features=env-logger-format
+
+exit 0
